64191acd835f55f0e9b03db7570598a8241edf9e
Thomas Fossati <thomas.fossati@linaro.org>
2026-05-22 16:56:36 +0200

    feat: add topology claim
    Add a "topology" claim to the top-level EAR to describe the composite
    attester internal arrangement.
    
    Signed-off-by: Thomas Fossati <thomas.fossati@linaro.org>

--- cddl/base-files.mk
+++ cddl/base-files.mk
@@ -1,6 +1,7 @@
 EAR_BASE_FRAGS += ear.cddl
 EAR_BASE_FRAGS += ear-appraisal.cddl
 EAR_BASE_FRAGS += claims-map.cddl
+EAR_BASE_FRAGS += topology.cddl
 
 EAR_BASE_EXAMPLES += examples/ear-cbor-1.diag
 EAR_BASE_EXAMPLES += examples/ear-json-1.diag
